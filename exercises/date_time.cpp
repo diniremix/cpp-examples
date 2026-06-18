9cf4c84b7a270f0aeb93905183fe0b89289fca08
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2026-06-17 19:13:53 -0500

    now use  format_tp() function

--- exercises/date_time.cpp
+++ exercises/date_time.cpp
@@ -30,15 +30,7 @@ namespace datetime {
     string format_zoned(auto zt)
     {
         auto local = zt.get_local_time();
-
-        auto dp = floor<chrono::days>(local);
-
-        chrono::year_month_day ymd{dp};
-
-        chrono::hh_mm_ss time{local - dp};
-
-        return fmt::format("{:04}-{:02}-{:02} {:02}:{:02}:{:02}", int(ymd.year()), unsigned(ymd.month()),
-                           unsigned(ymd.day()), time.hours().count(), time.minutes().count(), time.seconds().count());
+        return format_tp(local);
     }
 
     struct Date {
