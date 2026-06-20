569a1c5953405485647f258261e09a3aea1d55fa
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2026-06-19 20:17:07 -0500

    fixed exercises :fire:

--- xmake/xmake.lua
+++ xmake/xmake.lua
@@ -28,52 +28,43 @@ function add_exercise(name, opts)
     target_end()
 end
 
--- add_exercise("bin_files")
--- add_exercise("bin_pod_files")
+--
+add_exercise("bin_files")
+add_exercise("bin_pod_files")
 add_exercise("caesar_cipher")
-add_exercise(
-    "chars",
-      {
-          packages = {
-              "fmt",
-              "uni_algo"
-          }
-      }
-)
+add_exercise("chars",{
+    packages = {
+        "fmt",
+        "uni_algo"
+    }
+})
 add_exercise("classes")
--- -- add_exercise(
--- --     cli_app
--- --     LIBS
--- --       CLI11::CLI11
--- -- )
+add_exercise("cli_app",{
+        packages = { "cli11"}
+    }
+)
 add_exercise("compiler_info")
-add_exercise(
-    "coroutines_ext",
-        {
-            include = true,
-            packages = { "asio"}
-        }
+add_exercise("coroutines_ext",{
+        include = true,
+        packages = { "asio"}
+    }
 )
 add_exercise("coroutines_simple")
--- add_exercise("create_text_files")
--- add_exercise("csv_files")
+add_exercise("create_text_files")
+add_exercise("csv_files")
 add_exercise("factorial")
 add_exercise("fibonacci")
 add_exercise("fibonacci_rec")
 add_exercise("find_primes")
 add_exercise("fizzbuzz")
-add_exercise(
-    "formatter",
-    {
+add_exercise("formatter",{
         include = true,
         defines = { "HAS_FMT=1" },
         packages = { "fmt"}
     }
 )
 add_exercise("haversine")
-add_exercise(
-    "logger",
-    {
+add_exercise("logger",{
         packages = {
             "fmt",
             "spdlog"
@@ -88,45 +79,48 @@ add_exercise("morse",{
         "uni_algo"
     }
 })
-add_exercise(
-    "open_meteo",
-    {
-        packages = {
-            "fmt",
-            "cpr",
-            "nlohmann_json"
-        }
-    }
-)
+-- add_exercise(
+--     "open_meteo",
+--     {
+--         packages = {
+--             "fmt",
+--             "cpr",
+--             "nlohmann_json"
+--         }
+--     }
+-- )
 add_exercise("print")
--- add_exercise("read_text_files")
+add_exercise("read_text_files")
 add_exercise("regex_demo")
-add_exercise(
-    "rest_client",
-    {
+-- add_exercise("rest_client",{
+--         packages = {
+--             "fmt",
+--             "cpr",
+--             "nlohmann_json"
+--         }
+--     }
+-- )
+add_exercise("result_error")
+add_exercise("socket_client")
+add_exercise("socket_server")
+add_exercise("sqlite_demo", {
+    packages = {
+        "fmt",
+        "sqlite3",
+    }
+})
+add_exercise("sqlite_orm_demo",{
         packages = {
             "fmt",
-            "cpr",
-            "nlohmann_json"
+            "sqlitecpp",
+        }
+    })
+add_exercise("structs",{
+        include = true,
+        packages = {
+            "fmt"
         }
     }
-)
-add_exercise("result_error")
--- add_exercise("socket_client")
--- add_exercise("socket_server")
--- -- add_exercise(
--- --     sqlite_demo
--- --     LIBS
--- --         unofficial::sqlite3::sqlite3
--- -- )
--- -- add_exercise(
--- --     sqlite_orm_demo
--- --     LIBS
--- --         SQLiteCpp
--- -- )
-add_exercise(
-    "structs",
-    {include = true}
 )
 add_exercise("testing")
 add_exercise("thread_simple")
@@ -159,16 +153,15 @@ add_exercise("terminal")
 add_exercise("ranges")
 add_exercise("concepts")
 add_exercise("namespaces",  {include = true})
-add_exercise("date_time",
-    {
+add_exercise("date_time",{
         packages = {
+            "fmt",
             "date",
             "date-tz",
         }
     }
 )
-add_exercise("jwt",
-    {
+add_exercise("jwt",{
         include=true,
         packages = { "fmt", "jwt-cpp", "picojson", "libsodium"}
     }
